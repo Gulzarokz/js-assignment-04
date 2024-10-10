5bfc44a2fb5df1a63d456e53107bf1e0639fabc0
Gulzarokz <gokz22301@gamil.com>
2024-10-10 20:18:24 +0500

    add and practice chapter 9 and 10 prompt and if statment

---
+++ index.js
@@ -0,0 +1,82 @@
+// chapter NO 09 prompt
+
+// var userAge = prompt("Please Enter your Age");
+// console.log("Your are " + userAge+ " years old");
+
+
+// const userName = prompt("Please Enter user name ");
+// alert("Hello " + userName);
+// console.log("Hello " +userName);
+
+
+// const firstName = prompt("Enter your first Name");
+// const secondName = prompt("Enter your second Name")
+// const fullName = firstName + " " +secondName;
+// alert("Hello " + fullName);
+// console.log("Hello"  +fullName);
+
+
+// const pencil = prompt("enter pencil price");
+// const noteBook = prompt("enter noteBook");
+// const totalBill = pencil + noteBook;
+// console.log("Your Total Bill is = " +totalBill);
+
+
+// prompts always return string values that why i convert  to the numbers
+
+// const pencil = +prompt("enter pencil price");
+// const noteBook = +prompt("enter noteBook");
+// const totalBill = pencil + noteBook;
+// console.log("Your Total Bill is = " +totalBill);
+
+// chapter No 10 if statment
+
+
+// const badal = false;
+// if (badal){
+//     alert("take umbrella")
+// }else{
+//     alert("no badal")
+// }
+
+// var userAge = +prompt("Enter your age for driving licenses");
+// if(userAge >= 18){
+//     alert("you  are aligible to get driving license");
+// }else{
+//     alert("you  are not aligible to get driving license");
+// }
+
+// const userAge = +prompt("Enter your age for driving licence");
+// if(userAge >= 20){
+//     console.log("you are iligible for getting license");
+//     console.log("Here are we working with your license");
+//     console.log("Here are your license");
+    
+// }
+// if(userAge <= 18){
+//     console.log("your are not aligible for getting license");
+    
+// }
+// // brt
+// const userBalance = +prompt("Enter your balance ");
+// if(userBalance >= 50){
+//     alert("Welcome to zu peshawar")
+// }
+// if(userBalance <= 50){
+//     alert("please recharge ")
+// }
+
+// const userName = prompt("Enter user name");
+// const userNumber = prompt("please Enter number");
+
+// if(userNumber === 0){
+//     alert(`enter = 0 ` +userNumber);
+// }
+
+// if(userNumber >= 0){
+//     alert(`enter positibe number ` +userNumber);
+// }
+
+// if(userNumber <= 0){
+//     alert(`enter negative number ` +userNumber);
+// }
